fbab7cb5ec6e211f0fd6cc06ad813f499fbdbc65
michaelwinick <michaelwinick@outlook.com>
2022-09-09 16:49:15 -0700

    test now asserts against all return events

--- Clinic-test/UnitTest1.cs
+++ Clinic-test/UnitTest1.cs
@@ -3,7 +3,7 @@ using Account.Domain;
 using Clinic_test.Fixtures;
 using Eventuous;
 using Eventuous.TestHelpers.Fakes;
-
+using FluentAssertions;
 
 namespace Clinic_test;
 
@@ -24,15 +24,18 @@ public class UnitTest1 : NaiveFixture
     [Fact]
     public async Task Test1()
     {
-        Events.V1.PersonalAccountCreationStarted ev = new(Guid.NewGuid().ToString(), "Started", "Pumper");
-        var events = new List<Events.V1.PersonalAccountCreationStarted>
-        {
-            ev
-        };
+        Events.V1.PersonalAccountCreationStarted ev1 = new(Guid.NewGuid().ToString(), "Started", "Pumper");
+        Events.V1.PersonalAccountInformationAdded ev2 = new(ev1.AccountId, "FirstName", "LastName", "1/6/65", "InformationAdded");
+        
+        var streamName = new StreamName(GetStreamName(new AccountId(ev1.AccountId)));
 
-        var streamName = new StreamName(GetStreamName(new AccountId(ev.AccountId)));
-
-        var streamEvents = events.Select(x => new StreamEvent(Guid.NewGuid(), x, new Metadata(), "", 0));
+        var streamEvents = 
+            new List<object>
+            {
+                ev1, 
+                ev2
+            }.Select(e => 
+                new StreamEvent(Guid.NewGuid(), e, new Metadata(), "", 0));
 
         await EventStore.AppendEvents(
             streamName,
@@ -40,25 +43,42 @@ public class UnitTest1 : NaiveFixture
             streamEvents.ToArray(),
             default
         );
-        
+
         var expected = new Change[] {
-            new(new AccountCommands.AddPersonalAccountInformation(ev.AccountId, "FirstName", "LastName", "1/6/65"), 
-                "V1.")
+            new(
+                new Events.V1.PersonalAccountCreationStarted(ev1.AccountId.ToString(), "Started", "Pumper"),
+                "V1.PersonalAccountCreationStarted"
+                ),
+            new(
+                new Events.V1.PersonalAccountInformationAdded(ev1.AccountId, "FirstName", "LastName", "1/6/65", "InformationAdded"),
+                "V1.PersonalAccountInformationAdded"
+                ),
+            new(
+                new Events.V1.PersonalAccountCreated(ev1.AccountId, "email", "password", "securityQuestion", "securityAnswer", "healthDataNotice", "termsOfUse", "PersonalAccountCreated"),
+                "V1.PersonalAccountCreated"
+                )
         };
 
         var (accountState, success, enumerable) = await Service.Handle(
-            new AccountCommands.AddPersonalAccountInformation(
-                ev.AccountId,
-                "FirstName",
-                "LastName", 
-                "1/6/65"),
+            new AccountCommands.CompletePersonalAccount(
+                ev1.AccountId,
+                "email",
+                "password", 
+                "securityQuestion",
+                "securityAnswer",
+                "healthDataNotice",
+                "termsOfUse"),
             new CancellationToken());
 
         var readEvents = await EventStore.ReadEvents(
             streamName,
-            StreamReadPosition.Start, 2,
+            StreamReadPosition.Start, 100,
             new CancellationToken());
+
+        readEvents.Select(x => x.Payload)
+            .Should().BeEquivalentTo(expected.Select(x => x.Event));
     }
+
     static StreamName GetStreamName(AccountId accountId) => new($"Account-{accountId}");
 
 }
